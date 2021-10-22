d2c28f10c5e9e42cd3b86fcfe575658ae15ff997
hemkeshraina <2018.hemkesh.raina@ves.ac.in>
2021-10-22 20:07:18 +0530

    Update

---
+++ admin/config/constants.php
@@ -0,0 +1,7 @@
+<?php 
+    define('SITEURL','http://localhost:8080/quiztester/');
+    define('LOCALHOST','localhost');
+    define('USERNAME','root');
+    define('PASSWORD','');
+    define('DBNAME','quizapp');
+?>
---
+++ box/footer.php
@@ -0,0 +1,11 @@
+<!--Footer Starts Here-->
+        <footer class="footer">
+            <div class="wrapper">
+                <p>
+                    <a href="" title="ABC Institutes">Group 14</a> &copy; <?php echo date('Y'); ?>. All Rights Reserved.    
+                    Powered by <a href="" title="Vijay Thapa" target="_blank">Shubhra, Aaryan, Hemkesh</a>.
+                </p>
+            </div>
+        </footer>
+        <!--Footer Ends Here-->
+</html>
